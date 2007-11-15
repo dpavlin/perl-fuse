7210858b5f48edd70b4933f207435b96ba71bcc8
Dobrica Pavlinusic <dpavlin@rot13.org>
2007-11-15 09:33:07 +0000

    added Chris, bump version to 0.09
    git-svn-id: svn+ssh://llin/home/dpavlin/private/svn/fuse/perl-llin@113 6e4b0b00-1209-0410-87b2-b275959b5705

--- Fuse.pm
+++ Fuse.pm
@@ -28,7 +28,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = ();
-our $VERSION = '0.09_1';
+our $VERSION = '0.09';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
