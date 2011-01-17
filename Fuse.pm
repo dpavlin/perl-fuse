a61e1574405d8c647f79265adcb7dd184d24bd3a
Dobrica Pavlinusic <dpavlin@rot13.org>
2011-01-17 20:26:27 +0100

    bump version to 0.10_1

--- Fuse.pm
+++ Fuse.pm
@@ -28,7 +28,7 @@ our %EXPORT_TAGS = (
 our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
 
 our @EXPORT = ();
-our $VERSION = '0.09_4';
+our $VERSION = '0.10_1';
 
 sub AUTOLOAD {
     # This AUTOLOAD is used to 'autoload' constants from the constant()
